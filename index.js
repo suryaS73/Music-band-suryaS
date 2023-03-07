8ca98a36769113ec2f653045395dafd416e7769d
suryaS73 <125781536+suryaS73@users.noreply.github.com>
2023-03-07 14:38:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+function p()
+{
+     var p=new Audio()
+     p.src="./plate.mp3"
+     p.play()
+}
+function p2()
+{
+     var p=new Audio()
+     p.src="./plate2.mp3"
+     p.play()
+}
+function p3()
+{
+     var p=new Audio()
+     p.src="./plate3.wav"
+     p.play()
+}
+function first()
+{
+     var f=new Audio()
+     f.src="./first.wav"
+     f.play()
+}
+function first1()
+{
+     var f=new Audio()
+     f.src="./single.mp3"
+     f.play()
+}
+function middle()
+{
+     var m=new Audio()
+     m.src="./middle1.wav"
+     m.play()
+}
+function middle1()
+{
+     var m=new Audio()
+     m.src="./middle.wav"
+     m.play()
+}
+function last()
+{
+     var l=new Audio()
+     l.src="./last.mp3"
+     l.play()
+}
+function last1()
+{
+     var l=new Audio()
+     l.src="./snare hit.mp3"
+     l.play()
+}
